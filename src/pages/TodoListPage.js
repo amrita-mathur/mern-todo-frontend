89dee63263ce3d2f562e77f305100d6bded9f849
Amrita-code <amrita.mathur@greatlearning.in>
2023-07-21 09:11:47 +0530

    changed url again

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Todo from "../components/Todo";
 import { Container, Row, Col } from "react-bootstrap";
+import { useContext } from "react";
+import {TaskContext} from '../App';
 
 
 const TodoListPage = (props) => {
   const [todoList, setTodoList] = useState([]);
   const [error, setError] = useState("");
+  const { url } = useContext(TaskContext);
 
   useEffect(() => {
     axios
-      .get("http://localhost:3001/todos")
+      .get(url)
       .then((res) => setTodoList(res.data))
       .catch((err) => {
         setError(err.message);
